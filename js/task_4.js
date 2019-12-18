34ed98508dadd1e7730c1f9df4f9f740938d01c5
Mashina <mi.sidorenko@gmail.com>
2019-12-18 16:59:02 +0200

    added_hw_5_1_4

--- js/task_4.js
+++ js/task_4.js
@@ -11,14 +11,25 @@ get value() {
 }
 
 append(str){ 
-`${this._value} ${str}`;
+    this._value=`${this._value}${str}`;
 
 }
-prepend(str){}
-pad(str){}
+prepend(str){
+    this._value=`${str}${this._value}`;
+}
+pad(str){
+    this._value=`${str}${this._value}${str}`;
+}
 }
 
 const builder = new StringBuilder('.');
 
 builder.append('^');
 console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value);
+
+builder.pad('=');
+console.log(builder.value);
+
